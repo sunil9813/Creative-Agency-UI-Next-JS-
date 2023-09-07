60171c9e3d52370690b8b0cbaa974444365cffc8
rezaaditria <rezaaditria1@gmail.com>
2023-09-07 13:38:36 +0700

    styling

--- sections/Hero.js
+++ sections/Hero.js
@@ -1,39 +1,44 @@
-import { home } from "@/assets/data/dummydata"
-import Banner from "@/components/Banner"
-import Coins from "@/components/Coins"
-import Expertise from "@/components/Expertise"
-import ShowCase from "@/components/ShowCase"
-import Testimonial from "@/components/Testimonial"
-import { Title, TitleLogo, TitleSm } from "@/components/common/Title"
-import { BlogCard, Brand } from "@/components/router"
-import React from "react"
+import { home } from "@/assets/data/dummydata";
+import Banner from "@/components/Banner";
+import Coins from "@/components/Coins";
+import Expertise from "@/components/Expertise";
+import ShowCase from "@/components/ShowCase";
+import Testimonial from "@/components/Testimonial";
+import { Title, TitleLogo, TitleSm } from "@/components/common/Title";
+import { BlogCard, Brand } from "@/components/router";
+import React from "react";
 
 const Hero = () => {
   return (
     <>
-      <section className='hero'>
-        <div className='container'>
-          <TitleLogo  caption='Coins' title='MBC'  className='logobg' />
-          <h1 className='hero-title'>WE BUILD CRYPTOCURENCY ANALYSIS</h1>
+      <section className="hero">
+        <div className="container">
+          <TitleLogo caption="Coins" title="MBC" className="logobg" />
+          <h1 className="hero-title">CRYPTOCURRENCY</h1>
 
-          <div className='sub-heading'>
-            <TitleSm title='Multimedia' /> <span>.</span>
-            <TitleSm title='Big Data' /> <span>.</span>
-            <TitleSm title='Cybersecurity' /> 
+          <div className="sub-heading">
+            <TitleSm title="Multimedia" /> <span>.</span>
+            <TitleSm title="Big Data" /> <span>.</span>
+            <TitleSm title="Cybersecurity" />
           </div>
         </div>
       </section>
-      <section className='hero-sec'>
-        <div className='container'>
-          <div className='heading-title'>
-            <Title title='The last digital agency you ll ever need' />
-            <p>Suspendisse ut magna porttitor, sollicitudin ligula at, molestie dolor. Vivamus a ligula ut velit placerat egestas at id leo. Nulla ac volutpat nunc. Nulla facilisi. Pellentesque tempus tellusut magna porttitor scelerisque.</p>
+      <section className="hero-sec">
+        <div className="container">
+          <div className="heading-title">
+            <Title title="The last digital agency you ll ever need" />
+            <p>
+              Suspendisse ut magna porttitor, sollicitudin ligula at, molestie
+              dolor. Vivamus a ligula ut velit placerat egestas at id leo. Nulla
+              ac volutpat nunc. Nulla facilisi. Pellentesque tempus tellusut
+              magna porttitor scelerisque.
+            </p>
           </div>
           <Coins />
-          <div className='hero-content grid-4'>
+          <div className="hero-content grid-4">
             {home.map((item, i) => (
-              <div className='box' key={i}>
-                <span className='green'>{item.icon}</span> <br />
+              <div className="box" key={i}>
+                <span className="green">{item.icon}</span> <br />
                 <br />
                 <h3>{item.title}</h3>
               </div>
@@ -41,9 +46,8 @@ const Hero = () => {
           </div>
         </div>
       </section>
-      
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
