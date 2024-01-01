02db0818ff986b09a5004738c0e8946c48eabcd5
Rishi-sippy <sippyrishu@gmail.com>
2024-01-01 21:52:56 +0530

    changing links

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,102 +1,102 @@
-import Link from "next/link"
-import { TitleLogo } from "./Title"
-import { BsFacebook } from "react-icons/bs"
-import { AiFillBehanceCircle, AiFillInstagram, AiFillLinkedin } from "react-icons/ai"
+import Link from "next/link";
+import { TitleLogo } from "./Title";
+import { BsFacebook } from "react-icons/bs";
+import {
+  AiFillBehanceCircle,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <div className='container'>
-          <div className='grid-4'>
-            <div className='logo'>
-              <TitleLogo title='Sippy' caption='Rishi' className='logobg' />
+        <div className="container">
+          <div className="grid-4">
+            <div className="logo">
+              <TitleLogo title="Sippy" caption="Rishi" className="logobg" />
               <br />
               <span>
-                Questions? <br /> Reach to me  <br /> 24*7 available
+                Questions? <br /> Reach to me <br /> 24*7 available
               </span>
               <br />
               <br />
               <h3>+91 7018548653</h3>
+              <h3>sippyrishu@gmail.com</h3>
               <br />
-              <button className='button-primary'>Request for quote</button>
+              <button className="button-primary">Request for quote</button>
             </div>
             <ul>
               <h3>Quick Links</h3>
               <li>
-                <Link href='/'>About Me</Link>
+                <Link href="/">About Me</Link>
               </li>
               {/* <li>
                 <Link href='/'>Our team</Link>
               </li> */}
               <li>
-                <Link href='/'>Showcase</Link>
+                <Link href="/">Showcase</Link>
               </li>
               <li>
-                <Link href='/'>Blog</Link>
+                <Link href="/">Blog</Link>
               </li>
               <li>
-                <Link href='/'>Demo design system</Link>
+                <Link href="/">Demo design system</Link>
               </li>
               <li>
-                <Link href='/'>Contact</Link>
+                <Link href="/">Contact</Link>
               </li>
             </ul>
             <ul>
               <h3>SERVICES</h3>
               <li>
-                <Link href='/'>Web Design & Development</Link>
+                <Link href="/">Web Design & Development</Link>
               </li>
               <li>
-                <Link href='/'>App Design & Development</Link>
+                <Link href="/">App Design & Development</Link>
               </li>
               <li>
-                <Link href='/'>Branding & Creative Services</Link>
+                <Link href="/">Branding & Creative Services</Link>
               </li>
               <li>
-                <Link href='/'>Digital Marketing</Link>
+                <Link href="/">Digital Marketing</Link>
               </li>
               <li>
-                <Link href='/'>E-Commerce</Link>
+                <Link href="/">E-Commerce</Link>
               </li>
               <li>
-                <Link href='/'>SEO</Link>
+                <Link href="/">SEO</Link>
               </li>
               <li>
-                <Link href='/'>Paid Ads / PPC</Link>
+                <Link href="/">Paid Ads / PPC</Link>
               </li>
             </ul>
             <ul>
               <h3>CONNECT</h3>
-              <div className='connect'>
-                <li>
-                  <Link href='/'>
+              <div className="connect">
+                <Link href="https://www.facebook.com/rishi.k.sippy/">
+                  <li className="icon">
                     <BsFacebook size={25} />
-                  </Link>
-                </li>
-                <li>
-                  <Link href='/'>
-                    <AiFillBehanceCircle size={25} />
-                  </Link>
-                </li>
-                <li>
-                  <Link href='/'>
+                  </li>
+                </Link>
+                <Link href="#">
+                  <li className="icon">
                     <AiFillInstagram size={25} />
-                  </Link>
-                </li>
-                <li>
-                  <Link href='/'>
+                  </li>
+                </Link>
+                <Link href="https://www.linkedin.com/in/rishi-sippy-0785191b8/">
+                  <li className="icon">
                     <AiFillLinkedin size={25} />
-                  </Link>
-                </li>
+                  </li>
+                </Link>
               </div>
             </ul>
           </div>
-          <div className='legal connect py'>
-            <div className='text'>
+          <div className="legal connect py">
+            <div className="text">
               <span>© 2023 Rishi Sippy. ALL RIGHTS RESERVED.</span>
             </div>
-            <div className='connect'>
+            <div className="connect">
               <span>Rishi Sippy</span>
               <span> &nbsp; | &nbsp; </span>
               <span>TERMS & CONDITIONS</span>
@@ -105,7 +105,7 @@ const Footer = () => {
         </div>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- sections/Contact.js
+++ sections/Contact.js
@@ -1,97 +1,107 @@
-import { Title, TitleSm } from "@/components/common/Title"
-import React from "react"
-import { AiFillBehanceCircle, AiFillInstagram, AiFillLinkedin } from "react-icons/ai"
-import { BiUserCircle } from "react-icons/bi"
-import { BsFacebook } from "react-icons/bs"
-import { FiHeadphones, FiHelpCircle } from "react-icons/fi"
-import { IoLocationOutline } from "react-icons/io5"
+import { Title, TitleSm } from "@/components/common/Title";
+import Link from "next/link";
+import React from "react";
+import {
+  AiFillBehanceCircle,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
+import { BsFacebook } from "react-icons/bs";
+import { FiHeadphones, FiHelpCircle } from "react-icons/fi";
+import { IoLocationOutline } from "react-icons/io5";
 
 const Contact = () => {
   return (
     <>
-      <section className='contact bg-top'>
-        <div className='container'>
-          <div className='heading-title'>
-            <TitleSm title='CONTACT' /> <br />
+      <section className="contact bg-top">
+        <div className="container">
+          <div className="heading-title">
+            <TitleSm title="CONTACT" /> <br />
             <br />
-            <Title title="Let's start right now!" className='title-bg' />
+            <Title title="Let's start right now!" className="title-bg" />
           </div>
-          <div className='content py flex1'>
-            <div className='left w-30'>
-              <div className='contact-deatils'>
-                <div className='box'>
-                  <FiHeadphones size={30} className='icons' />
-                  <h3>1-001-234-5678</h3>
-                  <span>Call us: Mon - Fri 9:00 - 19:00</span>
+          <div className="content py flex1">
+            <div className="left w-30">
+              <div className="contact-deatils">
+                <div className="box">
+                  <FiHeadphones size={30} className="icons" />
+                  <h3>+91 7018548653</h3>
+                  <span>Call me: Mon - Fri 11:00 AM - 2:00 AM</span>
                 </div>
-                <div className='box'>
-                  <IoLocationOutline size={30} className='icons' />
-                  <h3>New York</h3>
-                  <span>990 Madison Ave, Midtown Manhattan, 2th Floor, NY 10022</span>
+                <div className="box">
+                  <IoLocationOutline size={30} className="icons" />
+                  <h3>Remote Developer</h3>
+                  <span>
+                    Working in piece environment to produce outstanding results
+                  </span>
                 </div>
-                <div className='box'>
-                  <FiHelpCircle size={30} className='icons' />
-                  <h3>info@dream-theme.com</h3>
-                  <span>Drop us a line anytime!</span>
+                <div className="box">
+                  <FiHelpCircle size={30} className="icons" />
+                  <h3>sippyrishu@gmail.com</h3>
+                  <span>Drop me a line anytime!</span>
                 </div>
-                <div className='box'>
-                  <BiUserCircle size={30} className='icons' />
-                  <h3>hr@dream-theme.com</h3>
-                  <span>Career at Seven Creative</span>
+                <div className="box">
+                  <BiUserCircle size={30} className="icons" />
+                  <h3>sippyrishu@gmail.com</h3>
+                  <span>Career with me</span>
                 </div>
               </div>
               <ul>
-                <li className='icon'>
-                  <BsFacebook size={25} />
-                </li>
-                <li className='icon'>
-                  <AiFillBehanceCircle size={25} />
-                </li>
-                <li className='icon'>
-                  <AiFillInstagram size={25} />
-                </li>
-                <li className='icon'>
-                  <AiFillLinkedin size={25} />
-                </li>
+                <Link href="https://www.facebook.com/rishi.k.sippy/">
+                  <li className="icon">
+                    <BsFacebook size={25} />
+                  </li>
+                </Link>
+                <Link href="#">
+                  <li className="icon">
+                    <AiFillInstagram size={25} />
+                  </li>
+                </Link>
+                <Link href="https://www.linkedin.com/in/rishi-sippy-0785191b8/">
+                  <li className="icon">
+                    <AiFillLinkedin size={25} />
+                  </li>
+                </Link>
               </ul>
             </div>
-            <div className='right w-70'>
-              <TitleSm title='Make an online enquiry' />
-              <p className='desc-p'>Got questions? Ideas? Fill out the form below to get our proposal. </p>
+            <div className="right w-70">
+              <TitleSm title="Make an online enquiry" />
+              <p className="desc-p">Get in touch with me for anything. </p>
 
               <form>
-                <div className='grid-2'>
-                  <div className='inputs'>
+                <div className="grid-2">
+                  <div className="inputs">
                     <span>Name</span>
-                    <input type='text' />
+                    <input type="text" />
                   </div>
-                  <div className='inputs'>
+                  <div className="inputs">
                     <span>Email</span>
-                    <input type='text' />
+                    <input type="text" />
                   </div>
                 </div>
-                <div className='grid-2'>
-                  <div className='inputs'>
+                <div className="grid-2">
+                  <div className="inputs">
                     <span>your budget</span>
-                    <input type='text' />
+                    <input type="text" />
                   </div>
-                  <div className='inputs'>
+                  <div className="inputs">
                     <span>timeframe</span>
-                    <input type='text' />
+                    <input type="text" />
                   </div>
                 </div>
-                <div className='inputs'>
-                  <span>TELL US A BIT ABOUT YOUR PROJECT*</span>
-                  <textarea cols='30' rows='10'></textarea>
+                <div className="inputs">
+                  <span>TELL ME A BIT ABOUT YOUR QUERY*</span>
+                  <textarea cols="30" rows="10"></textarea>
                 </div>
-                <button className='button-primary'>Submit</button>
+                <button className="button-primary">Submit</button>
               </form>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
